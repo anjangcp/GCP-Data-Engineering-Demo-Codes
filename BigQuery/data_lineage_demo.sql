a87aa0cc61c80e63833d791dcfb4ce47b910511b
Anjan <104628412+anjangcp@users.noreply.github.com>
2024-09-14 13:35:14 +0530

    Update data_lineage_demo.sql
    added comments in SQL code

--- BigQuery/data_lineage_demo.sql
+++ BigQuery/data_lineage_demo.sql
@@ -1,3 +1,9 @@
+/*
+Author : @ Anjan GCP Data Engineering
+Created SQLs to demo Biqguery Datalineage
+*/
+
+-- Create conslolidated sales table bi joining customer and items tables
 create or replace table data_eng_demos.cust_item_sales_dtls as
 SELECT
     customer.fname||''||customer.lname as customer_name,
@@ -10,6 +16,7 @@ SELECT
     INNER JOIN `gcp-dataeng-demo-431907.data_eng_demos.sales` AS sales ON customer.cust_id = sales.cust_id
     INNER JOIN `gcp-dataeng-demo-431907.data_eng_demos.items` AS items ON sales.item_id = items.item_id;
 
+-- Create Aggregate table based on customer name
 create or replace table data_eng_demos.customer_agg_sales as
 SELECT
   customer_name,
@@ -20,6 +27,7 @@ FROM
 GROUP BY
   1;
 
+-- Create Aggregate table based on item name
 create or replace table data_eng_demos.item_agg_sales as
 SELECT
   itm_name,
