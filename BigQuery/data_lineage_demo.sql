314a5cb49ba868d706329a6d6e273b90d8f632df
Anjan <104628412+anjangcp@users.noreply.github.com>
2024-09-14 13:32:15 +0530

    Create data_lineage_demo.sql
    new demo BQ SQL file

---
+++ BigQuery/data_lineage_demo.sql
@@ -0,0 +1,32 @@
+create or replace table data_eng_demos.cust_item_sales_dtls as
+SELECT
+    customer.fname||''||customer.lname as customer_name,
+    items.itm_name,
+    sales.qty,
+    sales.price,
+    sales.ord_date
+  FROM
+    `gcp-dataeng-demo-431907.data_eng_demos.customer` AS customer
+    INNER JOIN `gcp-dataeng-demo-431907.data_eng_demos.sales` AS sales ON customer.cust_id = sales.cust_id
+    INNER JOIN `gcp-dataeng-demo-431907.data_eng_demos.items` AS items ON sales.item_id = items.item_id;
+
+create or replace table data_eng_demos.customer_agg_sales as
+SELECT
+  customer_name,
+  SUM(qty) AS tot_qty,
+  SUM(price) AS tot_price
+FROM
+  data_eng_demos.cust_item_sales_dtls
+GROUP BY
+  1;
+
+create or replace table data_eng_demos.item_agg_sales as
+SELECT
+  itm_name,
+  SUM(qty) AS tot_qty,
+  SUM(price) AS tot_price
+FROM
+  data_eng_demos.cust_item_sales_dtls
+GROUP BY
+  1;
+
